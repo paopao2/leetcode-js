74f29b45f295594c669c6d69b237f5726f80de33
Chang Hu <ch2950@columbia.edu>
2015-04-05 23:25:29 -0700

    add Majority Element

---
+++ Majority Element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} num
+ * @return {number}
+ */
+var majorityElement = function(num) {
+    var i,
+        length = num.length,
+        max,
+        list = {};
+    if (length === 1) {
+        return num[0];
+    }
+    for (i = 0; i < length; i++) {
+        if (list[num[i]]) {
+            max = ++list[num[i]];
+            if (i > length/2 - 1 && max >= length/2) {
+                return num[i];
+            }
+        } else {
+            list[num[i]] = 1;
+        }
+    }
+    
+};
