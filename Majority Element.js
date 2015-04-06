b0ffdd091e0d2bf24008e2d94e3006d2aed1602f
Chang-Hu <ch2950@columbia.edu>
2015-04-05 23:40:56 -0700

    Update Majority Element.js

--- Majority Element.js
+++ Majority Element.js
@@ -1,4 +1,7 @@
 /**
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ * 
  * @param {number[]} num
  * @return {number}
  */
