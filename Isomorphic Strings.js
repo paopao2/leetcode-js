89023c639a995c93b4ee07179ece101038f78547
Chang-Hu <huchang0607@gmail.com>
2016-04-05 20:58:15 -0700

    Create Isomorphic Strings.js

---
+++ Isomorphic Strings.js
@@ -0,0 +1,50 @@
+/**
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+For example,
+Given "egg", "add", return true.
+
+Given "foo", "bar", return false.
+
+Given "paper", "title", return true.
+
+Note:
+You may assume both s and t have the same length.
+*/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    var len = s.length,
+        mapS = {},
+        mapT = {},
+        curS,
+        curT,
+        i;
+        
+    for (i = 0; i < len; i++) {
+        curS = s.charAt(i);
+        curT = t.charAt(i);
+        
+        if (!mapS.hasOwnProperty(curS)) {
+            mapS[curS] = curT;
+        } else if (mapS[curS] !== curT) {
+            return false;
+        }
+        
+        if (!mapT.hasOwnProperty(curT)) {
+            mapT[curT] = curS;
+        } else if (mapT[curT] !== curS) {
+            return false;
+        }
+    }
+    
+    return true;
+}; 
