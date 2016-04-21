44587023514f33575b77394c646cb9c0c5e401b9
Chang-Hu <huchang0607@gmail.com>
2016-04-20 21:41:00 -0700

    Create Construct Binary Tree from Inorder and Postorder Traversal.js

---
+++ Construct Binary Tree from Inorder and Postorder Traversal.js
@@ -0,0 +1,40 @@
+/**
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+    var iLen = inorder.length,
+        pLen = postorder.length;
+        
+    return getRoot(inorder, postorder, 0, iLen - 1, 0, pLen - 1);
+};
+
+function getRoot(inorder, postorder, iStart, iEnd, pStart, pEnd) {
+    if (iStart > iEnd || pStart > pEnd) {
+        return null;
+    }
+    
+    var value = postorder[pEnd],
+        node = new TreeNode(value),
+        index = inorder.indexOf(value),
+        i;
+        
+    node.left = getRoot(inorder, postorder, iStart, index - 1, pStart, pStart + index - iStart - 1);
+    node.right = getRoot(inorder, postorder, index + 1, iEnd, pStart + index - iStart, pEnd - 1);
+    
+    return node;
+}
