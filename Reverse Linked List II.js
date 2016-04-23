da23719801340de8e90f33be3844732d57791913
Chang Hu <ch2950@columbia.edu>
2016-04-22 20:24:48 -0700

    Update solution for reverse linked list II

--- Reverse Linked List II.js
+++ Reverse Linked List II.js
@@ -17,8 +17,25 @@ Given m, n satisfy the following condition:
  * }
  */
 
+/**
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
 
-//Memory Limit Exceeded
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
 
 /**
  * @param {ListNode} head
@@ -26,6 +43,7 @@ Given m, n satisfy the following condition:
  */
 var reverseBetween = function(head, m, n) {
     var dummy = new ListNode(0),
+        newTail,
         prev,
         cur = dummy,
         next,
@@ -36,26 +54,28 @@ var reverseBetween = function(head, m, n) {
     }
     dummy.next = head;
     i = m - 1;
+    
     while (i > 0) {
         cur = cur.next;
         i--;
     }
+    
     prev = cur;
     cur = cur.next;
+    newTail = prev.next;
+    
+    prev.next = null;
     i = n - m;
-    while (cur !== null && i > 0) {
+    
+    while (cur && i >= 0) {
         next = cur.next;
-        if (next) {
-            cur.next = next.next;
-            next.next = prev.next;
-            prev.next = next;
-            cur = cur.next;
-        } else {
-            cur.next = prev.next;
-            prev.next = cur;
-        }
+        cur.next = prev.next;
+        prev.next = cur;
+        cur = next;
         i--;
     }
+    
+    newTail.next = next;
     return dummy.next;
     
 };
