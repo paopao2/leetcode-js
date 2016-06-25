8513b9a498fed2c65e64185dd232c14a77d69a24
Chang Hu <ch2950@columbia.edu>
2016-06-25 16:09:58 -0700

    Add solution for Multiply Strings

--- Multiply Strings.js
+++ Multiply Strings.js
@@ -6,6 +6,44 @@ The numbers can be arbitrarily large and are non-negative.
 Converting the input string to integer is NOT allowed.
 You should NOT use internal library such as BigInteger.
 */
+
+// BETTER SOLUTION
+//  `num1[i] * num2[j]` will be placed at indices `[i + j`, `i + j + 1]` 
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    const len1 = num1.length;
+    const len2 = num2.length;
+    let result = new Array(len1 + len2).fill(0);
+    
+    for (let i = len1 - 1; i >= 0; i--) {
+        for (let j = len2 - 1; j >= 0; j--) {
+            let value = parseInt(num1.charAt(i)) * parseInt(num2.charAt(j));
+            result[i + j + 1] += value;
+            
+            if (result[i + j + 1] >= 10) {
+                result[i + j] += parseInt(result[i + j + 1] / 10);
+                result[i + j + 1] = result[i + j + 1] % 10;
+            }
+        }
+    }
+    
+    let string = '';
+    
+    for (let i = 0; i < result.length; i++) {
+        if (string === '' && result[i] === 0) {
+            continue;
+        }
+        
+        string += result[i];
+    }
+    
+    return string === '' ? '0' : string;
+};
+
 /**
  * @param {string} num1
  * @param {string} num2
